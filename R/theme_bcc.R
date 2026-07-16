07bc1139236d4a77b5a9059619fb2d985551d7ea
evelyn-greeves <evgreeves@gmail.com>
2026-07-16 16:37:17 +0100

    Add option to change base size of text in theme_bcc()

--- R/theme_bcc.R
+++ R/theme_bcc.R
@@ -3,6 +3,7 @@
 #' Creates a base theme for ggplot objects.
 #'
 #' @param base_family The font to use in this theme. Defaults to "Verdana"
+#' @param base_size Base font size, in pts. Defaults to 16
 #' @param gridline_x Should horizontal gridlines be applied? Defaults to TRUE
 #' @param gridline_y Should vertical gridlines be applied? Defaults to TRUE
 #' @param legend_position Position of legend in plot. One of "none", "top", "right", "bottom", "left" or "inside" - defaults to "right"
@@ -12,6 +13,7 @@
 #' @import ggplot2 sysfonts showtext
 #'
 theme_bcc <- function(base_family = "Verdana",
+                      base_size = 16,
                       gridline_x = T,
                       gridline_y = T,
                       legend_position = "right") {
@@ -52,7 +54,8 @@ theme_bcc <- function(base_family = "Verdana",
     ggplot2::element_blank()
   }
 
-  ggplot2::theme_classic(base_family = base_family) +
+  ggplot2::theme_classic(base_family = base_family,
+                         base_size = base_size) +
     ggplot2::theme(
       panel.background = ggplot2::element_blank(),
       panel.grid.major.x = gridline_x_major,
@@ -62,18 +65,18 @@ theme_bcc <- function(base_family = "Verdana",
       # set plot title elements
       plot.title = ggplot2::element_text(family = base_family,
                                          face="bold",
-                                         size = 28,
+                                         #size = 28,
                                          color = bcc_cols("black")),
       plot.title.position = "panel",
       # set plot subtitle/caption elements
       plot.subtitle = ggplot2::element_text(family = base_family,
                                             face="italic",
-                                            size = 22,
+                                            #size = 22,
                                             color = bcc_cols("black"),
                                             margin=ggplot2::margin(9,0,9,0)),
       plot.caption = ggplot2::element_text(family = base_family,
                                            face="italic",
-                                           size = 14,
+                                           #size = 14,
                                            color = bcc_cols("black")),
       plot.caption.position = "panel",
       # legend format
@@ -82,21 +85,21 @@ theme_bcc <- function(base_family = "Verdana",
       #                                      color = bcc_cols("black")),
       legend.title = ggplot2::element_blank(),
       legend.text = ggplot2::element_text(family = base_family,
-                                          size = 18,
+                                          #size = 18,
                                           color= bcc_cols("black")),
       legend.position = legend_position,
       # axis format
       axis.title = ggplot2::element_text(family = base_family,
-                                         size = 22,
+                                         #size = 22,
                                          color= bcc_cols("black")),
       axis.text = ggplot2::element_text(family = base_family,
-                                        size = 18,
+                                        #size = 18,
                                         color = bcc_cols("black")),
       axis.text.x = ggplot2::element_text(margin=ggplot2::margin(5, b = 10)),
       # facet strip format
       strip.background = ggplot2::element_rect(fill="white"),
       strip.text = ggplot2::element_text(family = base_family,
-                                         size  = 22,
+                                         #size  = 22,
                                          color = bcc_cols("black"))
     )
 }
